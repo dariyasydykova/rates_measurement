969efce56e7c1ac676676d1baefb91587f13c8d2
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2016-03-24 10:07:16 -0500

    Updating plots

--- src/plot_hyphy_rates.r
+++ src/plot_hyphy_rates.r
@@ -20,16 +20,16 @@ for (i in c(1:5)) {
 
   p_hyphy_rates <- ggplot() +
     geom_line(data=t2,aes(time,r),color="red") + 
-    geom_point(data=t1,aes(branch_len,rate),color="blue") +
+    geom_point(data=t1,aes(branch_len,rate),color="blue",alpha=0.5,size=0.9) +
     geom_line(data=t1,aes(branch_len,mean),color="blue") +
     xlab("Time") +
     ylab("Rate") +
-    scale_y_continuous(breaks=seq(-1,6,0.5), limits = c(-1,6)) +
-    scale_x_continuous(breaks=seq(0,2,0.5),limits = c(0,2),expand = c(0.01, 0)) +
+    scale_y_continuous(breaks=seq(-1,4.5,0.5), limits = c(-1,4.5)) +
+    scale_x_continuous(breaks=seq(0,0.5,0.1),limits = c(0,0.5),expand = c(0.01, 0)) +
     geom_hline(yintercept=1)+
     theme(axis.title = element_text(size = 14),
           axis.text = element_text(size = 12),
           legend.text = element_text(size = 11),
           legend.title = element_text(size = 12))
-  ggsave(paste0("plots/site",i,"num_rate_v_hyphy_rates.png"))
+  ggsave(paste0("plots/site",i,"_num_rate_v_hyphy_rates.png"))
 }
