d79f486a68995d479bc035a2a6c0b3d517fc6c0c
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2017-01-19 09:33:14 -0600

    adding aln tree files for hyphy input

--- src/make_aln_tree_files.sh
+++ src/make_aln_tree_files.sh
@@ -9,6 +9,6 @@ do
 	tree_file="${temp1}.tre"
 	temp2=`echo $aln_file | grep -oE 'n\d+_bl\d+\.\d+_site_dupl_\d+_\d+'`
 	aln_tree_file="${temp2}_aln_tree.txt"
-	echo cat $aln_file trees/$tree_file > $output_dir/$aln_tree_file
+	cat $aln_file trees/$tree_file > $output_dir/$aln_tree_file
 done
 
