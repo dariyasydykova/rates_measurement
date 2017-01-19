53badcb394d30d4796fbca49d8a9179a5e8f8038
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2017-01-19 09:41:59 -0600

    Adding inferred rates for site dupl

--- src/run_hyphy_site_repl_sim.sh
+++ src/run_hyphy_site_repl_sim.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 site_dupl_arr=(100 1000 10000 100000 1000000)
+num_sim=50
 
 tree_file=n2_bl0.24.tre
 
 for site_dupl in ${site_dupl_arr[*]}
 do	
-	for num in ${taxa_num_arr[*]}  
+	for i in $(seq 1 $num_sim) 
 	do
-		aln_tree_file=n2_bl0.24_site_dupl_${site_dupl}_aln_tree.txt
+		aln_tree_file=n2_bl0.24_site_dupl_${site_dupl}_${i}_aln_tree.txt
 		aln_tree_dir=aln_tree_files/site_dupl
-		rates_file=n2_bl0.24_site_dupl_${site_dupl}_rates.txt
+		rates_file=n2_bl0.24_site_dupl_${site_dupl}_${i}_rates.txt
 		echo "INFILE = ${aln_tree_dir}/${aln_tree_file}" > hyphy/setup.txt
 		echo "OUTFILE = ../inferred_rates/raw_rates/site_dupl/${rates_file}" >> hyphy/setup.txt
 		echo "SITE_DUPL = ${site_dupl}" >> hyphy/setup.txt
