0ec8b0786290b0ae2d0b4320d9c962f349c4b93e
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2017-01-19 09:19:04 -0600

    Adding alignments for testing site dupl v inference accuracy

---
+++ src/make_aln_tree_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+file_arr=(./aln/site_dupl/*)
+
+output_dir="hyphy/aln_tree_files/site_dupl"
+
+for aln_file in ${file_arr[*]} 
+do	 
+	temp1=`echo $aln_file | grep -oE 'n\d+_bl\d+\.\d+'`
+	tree_file="${temp1}.tre"
+	temp2=`echo $aln_file | grep -oE 'n\d+_bl\d+\.\d+_site_dupl_\d+'`
+	aln_tree_file="${temp2}_aln_tree.txt"
+	cat $aln_file trees/$tree_file > $output_dir/$aln_tree_file
+done
+
--- src/write_run_site_repl_sim.sh
+++ src/write_run_site_repl_sim.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 site_dupl_arr=(100 1000 10000 100000 1000000)
+num_sim=50
 total_sites=2 #sets the limit. This will only generate dupl for one site
 
 if [ ! -d "./aln" ]; then
@@ -11,11 +12,13 @@ if [ -f "./src/run_site_repl_sim.sh" ]; then
 fi
 
 tree_file=n2_bl0.24.tre
-
-for n in ${site_dupl_arr[*]}
+for site_dupl in ${site_dupl_arr[*]}
 do	
-	aln_file=n2_bl0.24_site_dupl_${n}.fa
-	echo "python src/simulate_aln.py 132L_A_foldx_ddG.txt trees/${tree_file} aln/site_dupl_aln/${aln_file} ${n} ${total_sites}" >> ./src/run_site_repl_sim.sh	
+	for i in $(seq 1 $num_sim) 
+	do
+	aln_file=n2_bl0.24_site_dupl_${site_dupl}_${i}.fa
+	echo python src/simulate_aln.py 132L_A_foldx_ddG.txt trees/$tree_file aln/site_dupl/$aln_file $site_dupl $total_sites >> ./src/run_site_repl_sim.sh	
+	done 
 done
 
 chmod +x ./src/run_site_repl_sim.sh
