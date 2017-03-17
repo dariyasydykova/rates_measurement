98e8a94ca6f97f480fbeb242033551f39e40802c
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2017-03-17 15:12:22 -0500

    Updating script names

--- src/simulate_aln.py
+++ src/simulate_aln.py
@@ -8,7 +8,7 @@ from Bio.Align import MultipleSeqAlignment
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
-def sim_mutSel_model(tree_file, aln_file, site_dupl, site_limit):
+def sim_mutSel_model(aln_type, tree_file, aln_file, site_dupl, site_limit):
 	'''
 	function simulates MutSel model for the number of site_dupl with num_model model types
 	'''
@@ -17,7 +17,14 @@ def sim_mutSel_model(tree_file, aln_file, site_dupl, site_limit):
 	parts = []	
 	site_lst=[2,3,4,5,6,7,8,9,10,11]					
 	for i in site_lst:
-		q_matrix_file="q_matrices/codon/site%s_q_matrix_132L_A.txt" %i
+		if aln_type=='aa':
+			q_matrix_file="q_matrices/aa/site%s_q_matrix_132L_A.txt" %i
+		elif aln_type=='codon:	
+			q_matrix_file="q_matrices/codon/site%s_q_matrix_132L_A.txt" %i
+		else:
+			print 'wrong alignment type!'
+			sys.exit()
+			
 		q_matrix=np.loadtxt(q_matrix_file)
 		model = Model("custom", {"matrix": q_matrix})		
 		
@@ -30,25 +37,26 @@ def sim_mutSel_model(tree_file, aln_file, site_dupl, site_limit):
 
 def main(argv):
 
-	if len(argv) != 5: # wrong number of arguments
+	if len(argv) != 6: # wrong number of arguments
 		print('''Usage:
-		simulate_aln.py <tree_file> <aln_file> <site_dupl_num> <site_limit>
+		simulate_aln.py <aa/codon> <tree_file> <aln_file> <site_dupl_num> <site_limit>
 		''')
 		sys.exit()
 		
-	tree_file = argv[1]
-	aln_file = argv[2]
+	aln_type = argv[1]
+	tree_file = argv[2]
+	aln_file = argv[3]
 	
 	# Define partition(s) 
-	site_dupl = int(argv[3]) # number of sites simulated under one model
-	site_limit = argv[4]
+	site_dupl = int(argv[4]) # number of sites simulated under one model
+	site_limit = argv[5]
 		
 	if site_limit == "all" or site_limit=="None":
 		site_limit == None
 	else:
 		site_limit = int(site_limit)
 	
-	sim_mutSel_model(tree_file, aln_file, site_dupl, site_limit)
+	sim_mutSel_model(aln_type, tree_file, aln_file, site_dupl, site_limit)
 		
 if __name__ == "__main__":
 	main(sys.argv)
